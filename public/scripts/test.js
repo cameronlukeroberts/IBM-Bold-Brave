a1c7a66c40f543e3739d81238daeb8d886eff0aa
mrBohrio <mttzpp98@gmail.com>
2017-07-31 16:38:58 +0100

    insert question in test

--- public/scripts/test.js
+++ public/scripts/test.js
@@ -1,13 +1,48 @@
-var activityMat, currentQuestion;
+var questionMat, currentQuestion;
 var number_question = 10;
+
+function randShuffle(data, l, r) //random shuffle di data [l, r)
+{
+  var len = 10;
+  while(len < r - l + 1) len *= 10;
+  var ite = 10;
+  for(var i=0; i<ite; i++)
+  {
+    var a = Math.floor(Math.random() * len)%(r-l) + l;
+    var b = Math.floor(Math.random() * len)%(r-l) + l;
+    var c = data[a];
+    data[a] = data[b];
+    data[b] = c;
+  }
+}
 
 function initTest()
 {
-  activityMat = Array(number_question);
-  for(var i=0;i<number_question;i++)
-    activityMat[i] = {description:"Descrizione della domanda numero "+(i+1), points: 0,positive:i%2==0};
-  currentQuestion = 0;
-  changeTest(0);
+  var result;
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      result = JSON.parse(this.responseText);
+    }
+  };
+  xhttp.open("GET", "/api/btq", false);
+  xhttp.send();
+  result.sort(function(a,b){a.category==b.category?0:(a.category<b.category?-1:1)});
+  questionMat = Array(number_question);
+  var l=0, ind=[2,2,3,3];
+  for(var r=1;r<result.length;++r)
+    if(result[l].category!=result[r].category){
+      randShuffle(result, l, r);
+      l=r;
+    }
+  randShuffle(result, l, result.length);
+  console.log(result);
+  for(var i=0,t=0;i<ind.length;i++)
+    for(var j=0;j<ind[i];j++)
+      questionMat[t++]={question:result[5*i+j].question,points:0,positive:result[5*i+j].positive};
+  console.log(questionMat);
+  currentQuestion=0;
+  changeTest();
 }
 
 function prevQuestion(){
@@ -18,7 +53,7 @@ function prevQuestion(){
 }
 
 function nextQuestion(){
-  if(activityMat[currentQuestion].points>0){
+  if(questionMat[currentQuestion].points>0){
     if(currentQuestion<number_question-1){
       currentQuestion++;
       changeTest();
@@ -29,22 +64,23 @@ function nextQuestion(){
 }
 
 function setAnswer(nAnswer){
-  activityMat[currentQuestion].points=nAnswer;
+  questionMat[currentQuestion].points=nAnswer;
   changeTest();
 }
 
 function changeTest()
 {
-  document.getElementById("questionDescription").innerHTML = activityMat[currentQuestion].description;
+  //alert(questionMat);
+  document.getElementById("questionDescription").innerHTML = questionMat[currentQuestion].question;
   for(var i=1;i<=5;i++)
     document.getElementById("activityOption"+i).style.backgroundColor="#FFFFFF";
-  if(activityMat[currentQuestion].points>0)
-    document.getElementById("activityOption"+activityMat[currentQuestion].points).style.backgroundColor="#F3F3F6";
+  if(questionMat[currentQuestion].points>0)
+    document.getElementById("activityOption"+questionMat[currentQuestion].points).style.backgroundColor="#F3F3F6";
 }
 
 function totalScore(){
   var score=0;
   for(var i=0;i<number_question;i++)
-    score+=(activityMat[i].positive || activityMat[0].points==0?activityMat[i].points:6-activityMat[i].points);
+    score+=(questionMat[i].positive || questionMat[0].points==0?questionMat[i].points:6-questionMat[i].points);
   alert(score);
 }
