d85e7e5ea3583edec074bc901429e6b9eae6c66b
Matilde <matilde.padovano@gmail.com>
2017-07-31 11:38:26 +0100

    .

--- public/scripts/PieChart.js
+++ public/scripts/PieChart.js
@@ -14,201 +14,43 @@ var m = 10,
   r = 80,
   z = d3.scale.category20c();
 
-var counte=0;
 var counter=["graf1","graf2","graf3","graf4","graf5"];
 
-data[0] = [levelsArr[0].perComp, 100 - levelsArr[0].perComp];
-// Insert an svg element (with margin) for each row in our dataset. A child g
-// element translates the origin to the pie center.
-var svg = d3.select("#"+(counter[counte++])).selectAll("svg")
-  .data(data)
-  .enter().append("svg")
-  .attr("width", (r + m) * 2)
-  .attr("height", (r + m) * 2)
-  .append("g")
-  .attr("transform", "translate(" + (r ) + "," + (r + m) + ")");
+for(var i=0;i<5;i++){
+  data[0] = [levelsArr[i].perComp, 100 - levelsArr[i].perComp];
+  // Insert an svg element (with margin) for each row in our dataset. A child g
+  // element translates the origin to the pie center.
+  var svg = d3.select("#"+(counter[i])).selectAll("svg")
+    .data(data)
+    .enter().append("svg")
+    .attr("width", (r + m) * 2)
+    .attr("height", (r + m) * 2)
+    .append("g")
+    .attr("transform", "translate(" + (r ) + "," + (r + m) + ")");
 
 
- var coun=-1;
-  svg.data(data).append("text")
-        .attr("dy", ".35em")
-        .attr("text-anchor", "middle")
-        .attr("font-size","30px")
-        .text(function(d) { coun++; var per=(data[coun][0]*100)/(data[coun][0]+data[coun][1]); return ~~per+"%"; })
+   var coun=-1;
+    svg.data(data).append("text")
+          .attr("dy", ".35em")
+          .attr("text-anchor", "middle")
+          .attr("font-size","30px")
+          .text(function(d) { coun++; var per=(data[coun][0]*100)/(data[coun][0]+data[coun][1]); return ~~per+"%"; })
 
-// The data for each svg element is a row of numbers (an array). We pass that to
-// d3.layout.pie to compute the angles for each arc. These start and end angles
-// are passed to d3.svg.arc to draw arcs! Note that the arc radius is specified
-// on the arc, not the layout.
-var colo=-1;
-var coun1=-1;
-svg.selectAll("path")
-  .data(d3.layout.pie())
-  .enter().append("path")
-  .attr("d", d3.svg.arc()
-    .innerRadius(r / 2)
-    .outerRadius(r))
-  .style("fill", function(d) {
-        colo++; //coun1++;
-        //if(coun1%2==0 && data[coun1][0]<data[coun1][1])colo=(colo%2)+1;
-        return z(colo%2); });
+  // The data for each svg element is a row of numbers (an array). We pass that to
+  // d3.layout.pie to compute the angles for each arc. These start and end angles
+  // are passed to d3.svg.arc to draw arcs! Note that the arc radius is specified
+  // on the arc, not the layout.
+  var colo=-1;
+  var coun1=-1;
+  svg.selectAll("path")
+    .data(d3.layout.pie())
+    .enter().append("path")
+    .attr("d", d3.svg.arc()
+      .innerRadius(r / 2)
+      .outerRadius(r))
+    .style("fill", function(d) {
+          colo++; //coun1++;
+          //if(coun1%2==0 && data[coun1][0]<data[coun1][1])colo=(colo%2)+1;
+          return z(colo%2); });
 
-
-
-
-
-
-
-data[0] = [levelsArr[1].perComp, 100 - levelsArr[1].perComp];
-var svg = d3.select("#"+(counter[counte++])).selectAll("svg")
-  .data(data)
-  .enter().append("svg")
-  .attr("width", (r + m) * 2)
-  .attr("height", (r + m) * 2)
-  .append("g")
-  .attr("transform", "translate(" + (r) + "," + (r + m) + ")");
-
-
- var coun=-1;
-  svg.data(data).append("text")
-        .attr("dy", ".35em")
-        .attr("text-anchor", "middle")
-        .attr("font-size","30px")
-        .text(function(d) { coun++; var per=(data[coun][0]*100)/(data[coun][0]+data[coun][1]); return ~~per+"%"; })
-
-// The data for each svg element is a row of numbers (an array). We pass that to
-// d3.layout.pie to compute the angles for each arc. These start and end angles
-// are passed to d3.svg.arc to draw arcs! Note that the arc radius is specified
-// on the arc, not the layout.
-var colo=-1;
-var coun1=-1;
-svg.selectAll("path")
-  .data(d3.layout.pie())
-  .enter().append("path")
-  .attr("d", d3.svg.arc()
-    .innerRadius(r / 2)
-    .outerRadius(r))
-  .style("fill", function(d) {
-        colo++; //coun1++;
-        //if(coun1%2==0 && data[coun1][0]<data[coun1][1])colo=(colo%2)+1;
-        return z(colo%2); });
-
-
-
-
-
-
-data[0] = [levelsArr[2].perComp, 100 - levelsArr[2].perComp];
-var svg = d3.select("#"+(counter[counte++])).selectAll("svg")
-  .data(data)
-  .enter().append("svg")
-  .attr("width", (r + m) * 2)
-  .attr("height", (r + m) * 2)
-  .append("g")
-  .attr("transform", "translate(" + (r) + "," + (r + m) + ")");
-
-
- var coun=-1;
-  svg.data(data).append("text")
-        .attr("dy", ".35em")
-        .attr("text-anchor", "middle")
-        .attr("font-size","30px")
-        .text(function(d) { coun++; var per=(data[coun][0]*100)/(data[coun][0]+data[coun][1]); return ~~per+"%"; })
-
-// The data for each svg element is a row of numbers (an array). We pass that to
-// d3.layout.pie to compute the angles for each arc. These start and end angles
-// are passed to d3.svg.arc to draw arcs! Note that the arc radius is specified
-// on the arc, not the layout.
-var colo=-1;
-var coun1=-1;
-svg.selectAll("path")
-  .data(d3.layout.pie())
-  .enter().append("path")
-  .attr("d", d3.svg.arc()
-    .innerRadius(r / 2)
-    .outerRadius(r))
-  .style("fill", function(d) {
-        colo++; //coun1++;
-        //if(coun1%2==0 && data[coun1][0]<data[coun1][1])colo=(colo%2)+1;
-        return z(colo%2); });
-
-
-
-
-
-
-
-
-
-
-data[0] = [levelsArr[3].perComp, 100 - levelsArr[3].perComp];
-var svg = d3.select("#"+(counter[counte++])).selectAll("svg")
-  .data(data)
-  .enter().append("svg")
-  .attr("width", (r + m) * 2)
-  .attr("height", (r + m) * 2)
-  .append("g")
-  .attr("transform", "translate(" + (r ) + "," + (r + m) + ")");
-
-
- var coun=-1;
-  svg.data(data).append("text")
-        .attr("dy", ".35em")
-        .attr("text-anchor", "middle")
-        .attr("font-size","30px")
-        .text(function(d) { coun++; var per=(data[coun][0]*100)/(data[coun][0]+data[coun][1]); return ~~per+"%"; })
-
-// The data for each svg element is a row of numbers (an array). We pass that to
-// d3.layout.pie to compute the angles for each arc. These start and end angles
-// are passed to d3.svg.arc to draw arcs! Note that the arc radius is specified
-// on the arc, not the layout.
-var colo=-1;
-var coun1=-1;
-svg.selectAll("path")
-  .data(d3.layout.pie())
-  .enter().append("path")
-  .attr("d", d3.svg.arc()
-    .innerRadius(r / 2)
-    .outerRadius(r))
-  .style("fill", function(d) {
-        colo++; //coun1++;
-        //if(coun1%2==0 && data[coun1][0]<data[coun1][1])colo=(colo%2)+1;
-        return z(colo%2); });
-
-
-
-
-
-data[0] = [levelsArr[4].perComp, 100 - levelsArr[4].perComp];
-var svg = d3.select("#"+(counter[counte++])).selectAll("svg")
-  .data(data)
-  .enter().append("svg")
-  .attr("width", (r + m) * 2)
-  .attr("height", (r + m) * 2)
-  .append("g")
-  .attr("transform", "translate(" + (r) + "," + (r + m) + ")");
-
-
- var coun=-1;
-  svg.data(data).append("text")
-        .attr("dy", ".35em")
-        .attr("text-anchor", "middle")
-        .attr("font-size","30px")
-        .text(function(d) { coun++; var per=(data[coun][0]*100)/(data[coun][0]+data[coun][1]); return ~~per+"%"; })
-
-// The data for each svg element is a row of numbers (an array). We pass that to
-// d3.layout.pie to compute the angles for each arc. These start and end angles
-// are passed to d3.svg.arc to draw arcs! Note that the arc radius is specified
-// on the arc, not the layout.
-var colo=-1;
-var coun1=-1;
-svg.selectAll("path")
-  .data(d3.layout.pie())
-  .enter().append("path")
-  .attr("d", d3.svg.arc()
-    .innerRadius(r / 2)
-    .outerRadius(r))
-  .style("fill", function(d) {
-        colo++; //coun1++;
-        //if(coun1%2==0 && data[coun1][0]<data[coun1][1])colo=(colo%2)+1;
-        return z(colo%2); });
+}
