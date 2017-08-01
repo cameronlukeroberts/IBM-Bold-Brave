722a0e36e07883611b903d18fe75cbfdd65684f9
Paolo Rizzo <paorizz98@gmail.com>
2017-08-01 15:46:45 +0100

    Authentication success

--- app.js
+++ app.js
@@ -16,6 +16,20 @@ var expressSession = require('express-session');
 
 var app = express();
 
+
+//Include the config file for DB information
+var config = require('./config');
+
+var Cloudant = require('cloudant');
+
+//Set up variables with database config information
+var user = config.db.username;
+var password = config.db.password;
+var host = config.db.host;
+
+//Create the cloudant object
+var cloudant = Cloudant("https://" + user + ":" + password + "@" + host);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -27,9 +41,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('express-session')({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false
+    secret: 'keyboard cat'
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -41,34 +53,42 @@ app.use('/api', api);
 
 
 // passport config
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
 passport.use('login', new LocalStrategy({
-    passReqToCallback : true
+    usernameField: 'username', // redundant, could override
+    passwordField: 'password', // same here
+    passReqToCallback: true
   },
-  function(req, username, password, done) {
-    // check in mongo if a user with username exists or not
-    User.findOne({ 'username' :  username },
-      function(err, user) {
-        // In case of any error, return using the done method
-        if (err)
-          return done(err);
-        // Username does not exist, log error & redirect back
-        if (!user){
-          console.log('User Not Found with username '+username);
-          return done(null, false,
-                req.flash('message', 'User Not found.'));
-        }
-        // User exists but wrong password, log the error
-        if (!isValidPassword(user, password)){
-          console.log('Invalid Password');
-          return done(null, false,
-              req.flash('message', 'Invalid Password'));
-        }
-        // User and password both match, return user from
-        // done method which will be treated like success
-        return done(null, user);
-      }
-    );
-}));
+  function(req, username, password, done){
+    console.log(username);
+    console.log(password);
+
+    var db = cloudant.db.use('bb_credentials');
+    db.find({selector:{username: username}}, function(er, result) {
+      if (er)
+        return done(er);
+      if (!result.docs[0])
+        return done(null, false);
+      console.log("ok");
+      if(result.docs[0].password == password)
+        return done(null, username);
+      else
+        return done(null, false);
+    });
+
+  }
+));
+
+
 /*
 passport.use('signup', new LocalStrategy({
     passReqToCallback : true
@@ -117,15 +137,6 @@ passport.use('signup', new LocalStrategy({
   });
 );
 */
-passport.serializeUser(function(user, done) {
-  done(null, user._id);
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
 
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,27 @@ var router = express.Router();
 
 var api = require('../api');
 
+var passport = require('passport');
 
+var isAuthenticated = function (req, res, next) {
+  console.log(req.user);
+  if (req.isAuthenticated())
+    return next();
+  res.redirect('/login');
+}
+
+var isAuthenticatedLogin = function (req, res, next) {
+  if (!req.isAuthenticated())
+    return next();
+  res.redirect('/');
+}
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', isAuthenticated, function(req, res, next) {
   res.render('index', { title: 'Dashboard' });
 });
 
 /* GET login page. */
-router.get('/login', function(req, res, next) {
+router.get('/login', isAuthenticatedLogin, function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
 
@@ -46,35 +59,20 @@ router.get('/activity/:lev/:mod', function(req, res, next) {
   res.render('activity', { title: 'Activities', level:req.params.lev, module:req.params.mod});
 });
 
-
-
-
 /* authentication */
 
-var passport = require('passport');
 
-/*
-router.post('/register', function(req, res) {
-    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
-        if (err) {
-            return res.render('register', { account : account });
-        }
-
-        passport.authenticate('singup')(req, res, function () {
-            res.redirect('/');
-        });
-    });
-});
-*/
+router.post('/login', passport.authenticate('login', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+    }
+));
 
 
-router.post('/login', passport.authenticate('login'), function(req, res) {
-    res.redirect('/');
-});
 
 router.get('/logout', function(req, res) {
     req.logout();
-    res.redirect('/');
+    res.redirect('/login');
 });
 
 router.get('/ping', function(req, res){
