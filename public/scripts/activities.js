75add861e8c0d3a37889ffbc415bee36232b7e2b
mrBohrio <mttzpp98@gmail.com>
2017-08-01 14:43:30 +0100

    changes to activity pages

--- public/scripts/activities.js
+++ public/scripts/activities.js
@@ -1,3 +1,24 @@
+function areYouSure(lv,mod,act,ans=-1){
+  var newHtml="";
+  if(ans==-1){
+    newHtml+="<div class='row' align='center'>Are you sure, Bro?</div>";
+    newHtml+="<div class='row'><div class='col-sm-1'></div>";
+    newHtml+="<div class='col-sm-4'><button type='button' onclick='areYouSure("+lv+","+mod+","+act+",1)' class='test-btn text-center btn-no-padding' id='activityOption1'>Yes</button></div>";
+    newHtml+="<div class='col-sm-2'></div>";
+    newHtml+="<div class='col-sm-4'><button type='button' onclick='areYouSure("+lv+","+mod+","+act+",0)' class='test-btn text-center btn-no-padding' id='activityOption1'>No</button></div>";
+    newHtml+="<div class='col-sm-1'></div></div>";
+  }
+  else if(ans==0){
+    newHtml +="<input type='checkbox' onclick='areYouSure("+lv+","+mod+","+act+")'>";
+    newHtml += "<label for='subscribeNews'>Activity completed</label> </div>";
+  }
+  else{
+    newHtml +="<input type='checkbox' disabled='disabled' onclick='areYouSure("+lv+","+mod+","+act+")' checked>";
+    newHtml += "<label for='subscribeNews'>Activity completed</label> </div>";
+  }
+  document.getElementById('completedCheck'+act).innerHTML = newHtml;
+}
+
 function initActivities(lv, mod)
 {
   if(lv > level)
@@ -15,7 +36,7 @@ function initActivities(lv, mod)
         newHtml += "<div class='panel-heading collapse-trigger'>"+activityCube[lv][mod][i].name;
         newHtml += "<span style='float: right'><i>"+activityCube[lv][mod][i].points+"Pt.</i></span></div>";
         newHtml += "<div class='panel-body box-collapsed collapsable'>"+activityCube[lv][mod][i].descr;
-        newHtml += "<div> <input type='checkbox'";
+        newHtml += "<div id='completedCheck"+i+"'> <input type='checkbox' onclick='areYouSure("+lv+","+mod+","+i+")' ";
         if(activityCube[lv][mod][i].completed) newHtml += "checked";
         newHtml += ">";
         newHtml += "<label for='subscribeNews'>Activity completed</label> </div>";
