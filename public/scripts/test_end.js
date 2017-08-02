c4aab75ffd813ad38021f4d70f43123f089d104c
Matilde <matilde.padovano@gmail.com>
2017-08-02 16:43:28 +0100

    grafico arriva fino a 100

--- public/scripts/test_end.js
+++ public/scripts/test_end.js
@@ -1,7 +1,7 @@
 function initTestEnd()
 {
   document.getElementById("score-section").innerHTML = "Your last score: "+bravometerData[bravometerData.length-1].score;
-  if(bravometerData[bravometerData.length-1].score<20){
+  if(bravometerData[bravometerData.length-1].score<40){
     document.getElementById("score-section").innerHTML = "<h3><b>Your last score:  "+bravometerData[bravometerData.length-1].score+"</b></h3></br>You're unsure of your bravery. Maybe you haven't been given opportunities to be bold, or maybe you're convinced that you're simply not a brave person. Either way, look for opportunities to improve how you do things. Take a more positive stance on taking on new opportunities, and actively seek out challenges that will boost your confidence and make you increasingly bolder.";
   }
   else if(bravometerData[bravometerData.length-1].score<80){
