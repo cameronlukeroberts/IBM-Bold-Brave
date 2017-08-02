35c8336545a73ba34d4b0863d902c98e439d9a27
Paolo Rizzo <paorizz98@gmail.com>
2017-08-02 16:48:52 +0100

    Asteroid

--- public/scripts/rocketrizzo.js
+++ public/scripts/rocketrizzo.js
@@ -4,11 +4,14 @@ var last;
 document.onkeydown = keyDown;
 document.onkeyup = keyUp;
 var canvas, context;
+var cwidth, cheight;
 var speed, velocity;
 var rocketImg, rocket;
+var asteroidImg;
+var asteroids, next_asteroid;
 window.onload = function(){
   canvas = document.getElementById('myCanvas');
-  var cwidth = canvas.width, cheight = canvas.height;
+  cwidth = canvas.width, cheight = canvas.height;
   //alert(cwidth+" "+cheight);
   context = canvas.getContext('2d');
   rocketImg = new Image();
@@ -17,24 +20,58 @@ window.onload = function(){
     context.drawImage(this, rocket.x, rocket.y, rocket.w, rocket.h);
   };
   rocketImg.src = '/imgs/rocket.png';
+  asteroidImg = new Image();
+  asteroidImg.src = '/imgs/asteroid.png';
   speed = cwidth/200;
   velocity = 0;
   last = new Date().getTime();
+  asteroids = new Array();
+  next_asteroid = Math.random()*1000/2+500;
   setInterval(loopGame, 1 / fps * 1000);
 }
 
 function loopGame(){
+  //update
   var delta = (new Date().getTime() - last)/(1000/fps);
+  next_asteroid -= (new Date().getTime() - last);
   last = new Date().getTime();
 
   rocket.x+=velocity*delta;
   if(rocket.x<0)
     rocket.x=0;
   if(rocket.x+rocket.w>canvas.width)
-    rocket.x=canvas.width-rocket.w
-  context.clearRect(0, 0, canvas.width, canvas.height);
+    rocket.x=canvas.width-rocket.w;
 
+  if(next_asteroid<0){
+    var ast = {x: rand(0, cwidth), y: 0, speed: rand(speed/2, 2*speed), w: rand(cwidth/10, cwidth/4), h: 0};
+    ast.h = ast.w;
+    ast.y = -ast.h;
+    if(ast.x+ast.w > cwidth)
+      ast.x = cwidth-ast.w;
+    asteroids.push(ast);
+    next_asteroid = Math.random()*1000/2+500;
+    //console.log("asteroideeee");
+  }
+  var q = new Array();
+  for(var i=0;i<asteroids.length;i++){
+    asteroids[i].y+=speed;
+    if(asteroids[i].y>cheight){
+      q.push(i);
+    }
+  }
+
+  while(q.length>0){
+    asteroids.splice(q[q.length-1], 1);
+    q.pop();
+  }
+
+
+  //redraw
+  context.clearRect(0, 0, canvas.width, canvas.height);
   context.drawImage(rocketImg, rocket.x, rocket.y, rocket.w, rocket.h);
+  for(var i=0;i<asteroids.length;i++)
+    context.drawImage(asteroidImg, asteroids[i].x, asteroids[i].y, asteroids[i].w, asteroids[i].h);
+
 }
 
 function keyDown(e){
@@ -56,4 +93,10 @@ function keyUp(e){
     if(velocity > 0)
       velocity = 0;
   }
+}
+
+function rand(l, r){ //[l,r)
+  var len = 10;
+  while(len < r - l + 1) len *= 10;
+  return Math.floor(Math.random() * len)%(r-l) + l;
 }
---
+++ public/stylesheets/index.js
@@ -0,0 +1,151 @@
+var express = require('express');
+var router = express.Router();
+
+var api = require('../api');
+
+var passport = require('passport');
+
+var multer = require('multer');
+var bodyParser = require('body-parser');
+
+
+var Storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, "./public/profiles");
+    },
+    filename: function(req, file, callback) {
+        //callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+        var ext = require('path').extname(file.originalname);
+        //var size = require('fs').stat(file.path).size;
+
+        if(ext == '.png' || ext == '.jpg' || ext== '.jpeg'){
+          if(api.upload_image(req, ext))
+            callback(null, ""+req.user+ext);
+          else
+            callback("db", false);
+        }else
+          callback("ext", false);
+
+    }
+});
+
+
+var upload = multer({
+    storage: Storage
+}).array("image", 3); //Field name and max count
+
+
+var isAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated())
+    return next();
+  res.redirect('/login');
+}
+
+var isAuthenticatedLogin = function (req, res, next) {
+  if (!req.isAuthenticated())
+    return next();
+  res.redirect('/');
+}
+/* GET home page. */
+router.get('/', isAuthenticated, function(req, res, next) {
+  //console.log(req.user);
+  res.render('index', { title: 'Dashboard', user: req.user });
+});
+
+/* GET profile page. */
+router.get('/profile/:message', isAuthenticated, function(req, res, next) {
+  console.log(req.params.message)
+  res.render('profile', { title: 'Profile', user: req.user, message: req.params.message });
+});
+
+router.get('/profile', isAuthenticated, function(req, res, next) {
+  res.render('profile', { title: 'Profile', user: req.user, message: "undefined" });
+});
+
+
+router.get('/rocketrizzo', function(req, res, next) {
+  res.render('rocketrizzo', { title: 'Rocket', user: req.user });
+});
+
+/* GET help page. */
+router.get('/help', isAuthenticated, function(req, res, next) {
+  res.render('help', { title: 'Help', user: req.user });
+});
+
+/* GET test page. */
+router.get('/test', isAuthenticated, function(req, res, next) {
+  res.render('test', { title: 'Brave&Bold Test', user: req.user });
+});
+
+/* GET test_start page. */
+router.get('/test_start', isAuthenticated, function(req, res, next) {
+  res.render('test_start', { title: 'Brave&Bold Test', user: req.user });
+});
+
+
+/* GET test_end page. */
+router.get('/test_end', isAuthenticated, function(req, res, next) {
+  res.render('test_end', { title: 'Brave&Bold Test', user: req.user });
+});
+
+
+/* GET activity page. */
+router.get('/activity/:lev/:mod', isAuthenticated, function(req, res, next) {
+  res.render('activity', { title: 'Activities', level:req.params.lev, module:req.params.mod, user: req.user });
+});
+
+/* GET login page. */
+router.get('/login', isAuthenticatedLogin, function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+/* authentication */
+router.post('/login', passport.authenticate('login', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+    }
+));
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/login');
+});
+
+router.get('/register', isAuthenticatedLogin, function(req, res, next){
+  res.render('register', {title: 'Register'});
+});
+
+router.post('/register', function(req, res, next){
+  console.log("Registering");
+  api.register_user(
+    req.body.username,
+    req.body.name,
+    req.body.password,
+    req.body.password_confirm,
+    req.body.img).then(function(resp){
+      res.redirect('/login');
+    }).catch(function(err){
+      res.send(err);
+    });
+});
+
+router.get('/ping', function(req, res){
+    res.status(200).send("pong!");
+});
+
+router.post("/uploadImage", function(req, res) {
+    upload(req, res, function(err) {
+        console.log(err);
+        if (err) {
+            return res.redirect('/profile/'+err);
+        }
+
+        return res.redirect('/profile/success');
+    });
+});
+/*
+router.get('/imgs/:img', isAuthenticatedLogin, function(req, res, next){
+  res.sendFile('./uploads/'+req.params.img);
+});
+*/
+module.exports = router;
